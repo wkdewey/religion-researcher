d90b94cbf22c6c638391eae5ea61b1da57e8caa0
wkdewey <wkdewey@gmail.com>
2020-08-04 16:02:49 -0400

    fix signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :researchers
-  get '/signin', to: 'researchers#signin'
+  get '/signup', to: 'researchers#new'
   get '/login', to: 'researchers#login'
   get '/logout', to: 'researchers#logout'
 end
