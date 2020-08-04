c09170b7f4a29cf5705c5bd103fb1157f36a2e88
wkdewey <wkdewey@gmail.com>
2020-08-04 16:11:07 -0400

    add methods to create and log in researcher

--- app/controllers/researchers_controller.rb
+++ app/controllers/researchers_controller.rb
@@ -4,7 +4,17 @@ class ResearchersController < ApplicationController
   end
 
   def create
-    
+    @researcher = Researcher.new(researcher_params)
+    if @researcher.save
+      login_researcher(@researcher)
+      redirect_to @researcher
+    else
+      render :new
+    end
+  end
+
+  def show
+
   end
 
   def login
@@ -13,5 +23,15 @@ class ResearchersController < ApplicationController
 
   def logout
 
+  end
+
+  private
+
+  def researcher_params
+    params.require(:researcher).permit(:name, :email, :institution, :password)
+  end
+
+  def login_researcher(@researcher)
+    session[:researcher_id] = @researcher
   end
 end
