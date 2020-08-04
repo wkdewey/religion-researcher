29904b11a48c7994ca0bb6f66c8803c9e016281b
wkdewey <wkdewey@gmail.com>
2020-08-04 17:28:03 -0400

    add logic for lcreate/post login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
-    
   end
 
   def create
-
+    
+    @researcher = Researcher.find_by(name: params[:name])
+    if @researcher && @researcher.authenticate(params[:password])
+      sessions[:researcher_id] = @researcher.id
+      redirect_to @researcher
+    else
+      redirect_to '/login'
+    end
   end
 
 end
