992780b600cd29f43b60594fb728a02777d7ebde
wkdewey <wkdewey@gmail.com>
2020-08-05 13:49:27 -0400

    fix researcher show view and add project routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#welcome'
   resources :researchers
+  resources :projects
   get '/signup', to: 'researchers#new'
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
