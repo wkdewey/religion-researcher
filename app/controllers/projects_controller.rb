c8388131e9c1b577cdc5fef0572b208c69b6d182
wkdewey <wkdewey@gmail.com>
2020-08-05 16:19:32 -0400

    create project update action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,4 +9,14 @@ class ProjectsController < ApplicationController
     @texts = Text.all
   end
 
+  def update
+    @project = Project.find_by(id: params[:id])
+    @project.update(project_params)
+    redirect_to project_path(@project)
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :text_ids)
+  end
+
 end
