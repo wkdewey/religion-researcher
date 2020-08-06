c0e806b111858237df15a9ab634466932e29d9c1
wkdewey <wkdewey@gmail.com>
2020-08-06 16:50:21 -0400

    make create and edit actions/views for texts

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -7,6 +7,7 @@ class TextsController < ApplicationController
   def new
     @text = Text.new
     @authors = Author.all
+    @religious_tradition = ReligiousTradition.all
   end
 
   def create
@@ -17,6 +18,19 @@ class TextsController < ApplicationController
   def show
     @text = Text.find_by(id: params[:id])
   end
+  
+  def edit
+    @text = Text.find_by(id: params[:id])
+    @authors = Author.all
+    @religious_tradition = ReligiousTradition.all
+  end
+
+  def update
+    @text = Text.find_by(id: params[:id])
+    @text.update(text_params)
+    redirect_to text_path(@text)
+  end
+
 
   private
 
