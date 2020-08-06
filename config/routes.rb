471f36a76f0a51868a6870c81b0389b5783e4972
wkdewey <wkdewey@gmail.com>
2020-08-06 09:51:25 -0400

    fix gems and route for omniauth github signin

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get '/auth/:provider/callback', to: 'sessions#create_omniauth'
+  match '/auth/:provider/callback', to: 'sessions#create_omniauth', via: [:get, :post]
 end
