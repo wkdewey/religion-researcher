1e3d3c4d3244de20a72492e71c7c467a35916610
wkdewey <wkdewey@gmail.com>
2020-08-09 14:18:14 -0400

    add helper methods for text author in forms

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,3 +1,9 @@
 module TextsHelper
-  
+  def author_id_field(song)
+    if song.author.nil?
+      select_tag "text[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+    else
+      hidden_field_tag "text[author_id]", song.artist_id
+    end
+  end
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -5,4 +5,13 @@ class Text < ApplicationRecord
   validates :title, presence: true
   accepts_nested_attributes_for :author
   accepts_nested_attributes_for :notes
+
+  def author_name
+    self.try(:author).try(:name)
+  end
+
+  def author_name=(name)
+    author = Author.find_or_create_by(name: name)
+    self.author = author
+  end
 end
