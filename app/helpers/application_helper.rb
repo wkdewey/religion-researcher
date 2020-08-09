e079c53b469fdfe3d54f400e041be3c9695d7de4
wkdewey <wkdewey@gmail.com>
2020-08-09 11:30:44 -0400

    add navigational links when logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_researcher
+    @current_researcher ||= Researcher.find_by(id: session[:researcher_id])
+  end
+
+  def logged_in?
+    !!session[:researcher_id]
+  end
 end
