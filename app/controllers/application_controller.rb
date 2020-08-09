576391aa146c92ca2900f63934b7e4ac60585a6e
wkdewey <wkdewey@gmail.com>
2020-08-09 19:16:14 -0400

    delete unused methods from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,6 @@ class ApplicationController < ActionController::Base
     session[:researcher_id] = researcher.id
   end
 
-  def current_researcher
-    @current_researcher ||= Researcher.find_by(id: session[:researcher_id])
-  end
-
-  def logged_in?
-    !!session[:researcher_id]
-  end
-
-  def login_error
-    flash[:error] = "You must log in to see that page"
-    redirect_to "/login"
-  end
-
   def require_login
     return head(:forbidden) unless logged_in?
   end
