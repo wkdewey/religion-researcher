0b7053068efdf93ad48394471bb286f39e8dbe6b
wkdewey <wkdewey@gmail.com>
2020-08-10 17:52:00 -0400

    remove unused scope

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,6 @@ class Note < ApplicationRecord
   belongs_to :project
   belongs_to :text
   scope :written, -> { where.not(content: nil) }
-  scope :blank, -> { where(content: nil) }
 
   def belongs_to?(project)
     self.project_id == project.id
