8421267ef7bfb12e4176d7cda55dd126ac051f08
wkdewey <wkdewey@gmail.com>
2021-02-15 16:21:53 -0500

    change database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_08_04_004812) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "religious_tradition_id", null: false
+    t.bigint "religious_tradition_id", null: false
     t.index ["religious_tradition_id"], name: "index_authors_on_religious_tradition_id"
   end
 
@@ -56,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_004812) do
     t.string "subject"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_texts_on_author_id"
   end
 
