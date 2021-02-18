d5ce7f8463cb1c9f3c0bf93e01e2ec0887cb4bee
wkdewey <wkdewey@gmail.com>
2021-02-18 15:47:42 -0500

    configure rails project for bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,3 +14,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// import the bootstrap javascript module
+import "bootstrap";
+
+// import the application.scss we created for the bootstrap CSS (if you are not using assets stylesheet)
+import "../stylesheets/application";
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
 
-module.exports = environment
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"], // for Bootstrap 4
+  })
+);
+module.exports = environment;
